c6675cb777b59d63575bb0b902f8e1664f1f82e1
MarsTechHAN <hansh-sz@hotmail.clm>
2025-10-14 01:35:36 +0800

    Update API to change default port from 8000 to 5000 for backend server

--- backend/api.py
+++ backend/api.py
@@ -413,5 +413,5 @@ async def background_fetcher():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
 
